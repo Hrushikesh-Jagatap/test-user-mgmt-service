c364a8c5b7636ca55b3e3c15bf3a895d7bde177d
amit00986 <aryan.amit0824@gmail.com>
2023-12-02 14:13:40 +0530

    changes

--- src/apis/models/index.js
+++ src/apis/models/index.js
@@ -1,9 +1,11 @@
 const Users = require('./Users');
 const PrivateTution = require('./PrivateTut');
 const HomeTuition = require('./HomeTut');
+const contact = require('./Contact');
 
 module.exports = {
   Users,
   PrivateTution,
-  HomeTuition
+  HomeTuition,
+  contact
 };
